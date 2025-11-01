62f9e0596637188d2560985658bda9e02050973c
Hadi Rahjou <rahjooh@yahoo.com>
2025-11-01 10:15:34 +0000

    fix : provider.tf

--- provider.tf
+++ provider.tf
@@ -2,21 +2,12 @@ provider "aws" {
   region = var.region
 
   assume_role {
-    role_arn = var.assume_role_arn != null ? var.assume_role_arn : ""
+    role_arn = coalesce(var.assume_role_arn, "arn:aws:iam::640168415309:role/MSK-Builder")
     # tags = { AccessScope = "team-x" }  # if your org uses ABAC session tags
   }
 
   default_tags {
     tags = var.tags
-  }
-}
-
-provider "aws" {
-  alias  = "untagged"
-  region = var.region
-
-  assume_role {
-    role_arn = var.assume_role_arn != null ? var.assume_role_arn : ""
   }
 }
 
